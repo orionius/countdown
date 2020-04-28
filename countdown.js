642eb181a864b7352f0246d99e68f86bb1a53922
orionius <citrix@mail.ru>
2020-04-29 00:15:19 +0300

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,99 +1,87 @@
 window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
 
-    // Задаём конечную дату (Дедлайн)
-    let deadline = '2019-04-22';
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
 
-    // Сколько осталось времени
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+
+    });
+
+    // Timer 
+
+    let deadline = '2022-11-21';
+
     function getTimeRemaining(endtime) {
-        // Получаем разницу. Переводим конечную дату в миллисекунды и отнимаем текущую дату
-        let t = Date.parse(endtime) - Date.now(),
-            // Высчитываем секунды, минуты, часы и дни
+        let t = Date.parse(endtime) - Date.parse(new Date()),
             seconds = Math.floor((t / 1000) % 60),
             minutes = Math.floor((t / 1000 / 60) % 60),
-            hours = Math.floor((t / 1000 / 60 / 60) % 24),
-            days = Math.floor((t / 1000 / 60 / 60 / 24));
+            hours = Math.floor((t / (1000 * 60 * 60)));
 
         return {
             'total': t,
-            'seconds': seconds,
-            'minutes': minutes,
             'hours': hours,
-            'days': days
+            'minutes': minutes,
+            'seconds': seconds
         };
     }
 
-    // Форматируем и устанавливаем значения в разметку
     function setClock(id, endtime) {
-        // Получаем блок таймера по его id
         let timer = document.getElementById(id),
-            // И соотвественно, все элементы блока
-            days = timer.querySelector('.days'),
-            dayname = timer.querySelector('.dayname'),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
-            // Обновляем таймер каждую секунду
             timeInterval = setInterval(updateClock, 1000);
 
-        // Функция для обновления таймера
         function updateClock() {
-            // Помещаем в t результат функции getTimeRemaining (объект)
             let t = getTimeRemaining(endtime);
-            // Если дней осталось 0 - не выводим
-            if (t.days <= 0) {
-                days.textContent = "";
-                dayname.textContent = "";
-            } else {
-                days.textContent = t.days;
-                // Вставляем слово "день" при этом форматируя (дней, дня)
-                dayname.textContent = formatDays(t.days);
-            }
-            // Вставляем оставшееся время
-            hours.textContent = formatTime(t.hours);
-            minutes.textContent = formatTime(t.minutes);
-            seconds.textContent = formatTime(t.seconds);
 
-            // Если Дедлайн прошёл - вставляем в таймер 00:00:00,
-            // и останавливаем отсчёт (clearInterval)
+            function addZero(num) {
+                if (num <= 9) {
+                    return '0' + num;
+                } else return num;
+            };
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
             if (t.total <= 0) {
                 clearInterval(timeInterval);
-                days.textContent = "";
                 hours.textContent = '00';
                 minutes.textContent = '00';
                 seconds.textContent = '00';
             }
         }
 
-        // Функция добавляет 0 к единцам, получаем
-        // 03:04:05 вместо 3:4:5
-        function formatTime(time) {
-            if (time < 10) {
-                time = '0' + time;
-            }
-            return time;
-        }
-
-        // Форматируем слово день в зависимости от числа
-        // 1 день, 2 дня, 5 дней.
-        function formatDays(day) {
-            let sb = '',
-                dayNew = day % 100,
-                lastFigure = dayNew % 10;
-            if (dayNew > 10 && dayNew < 20) {
-                sb = 'ей';
-            } else if (lastFigure == 1) {
-                sb = "день";
-            } else if (lastFigure > 1 && lastFigure < 5) {
-                sb = 'дня';
-            } else {
-                sb = 'дней';
-            }
-            return sb;
-        }
     }
 
-    //Вызов таймера. Передаём id элемента и наш Дедлайн
     setClock('timer', deadline);
 });
